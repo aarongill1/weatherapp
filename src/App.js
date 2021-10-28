a4d10213ce69e8a56023fdf1753123818b255ca4
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2021-10-28 21:32:57 +0100

    remove unrequired files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './pagestyles.css'
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const [searchCompleted, setSearchCompleted] = useState(false);
+
+  const getTemp = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city || "london"}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+      .then(json => json.json())
+      .then(json => {
+        setTemp(json.main.temp);
+        setCity(json.name);
+      })
+      .catch(err => console.log(err))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>What's the Fucking Temperature?!?</h1>
+      <form>
+        <input type="text" value={city} placeholder="city" onChange={e => {
+          setSearchCompleted(false);
+          setCity(e.target.value);
+        }
+        }/>
+        <button onClick={e => {
+          e.preventDefault();
+          getTemp();
+          setCity("");
+          setSearchCompleted(true);
+        }}>Click
+        </button>
+      </form>
+      {searchCompleted &&
+      <div>
+        <h2>The temperature in {city} is:</h2>
+        <h1>{temp}</h1>
+      </div>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
